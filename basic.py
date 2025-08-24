fdef4491962dd8334e672a854c7c7c34515a2466
abhimaan9948 <abhimaan8499@gmail.com>
2025-08-25 01:23:05 +0530

    basic program 2

--- basic.py
+++ basic.py
@@ -6,4 +6,19 @@ for i in range(1,100):
     else:
         print("The given no is Odd")
 
+# Write a table of 10 in full format like 10 x 1 = 10 and so on .
+
+for i in range(1,10):
+    print("10 x",i,"=",10*i)
+
+    
+    
+
+    
+    
+    
+
+    
+
+
     
