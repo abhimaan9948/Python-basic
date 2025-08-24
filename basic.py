e75019c2c6d991cca52aa1c02480d8dcbfffba98
abhimaan9948 <abhimaan8499@gmail.com>
2025-08-25 01:07:10 +0530

    this is basic program

--- basic.py
+++ basic.py
@@ -1,1 +1,9 @@
-print("ABHIMAN")
+# to check whether a number is odd or even in a range 1 to 100:
+
+for i in range(1,100):
+    if i%2==0:
+        print("The given no is Even")
+    else:
+        print("The given no is Odd")
+
+    
