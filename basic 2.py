03c861d08d7292a47f9eb4a7ebf19ab930f6b689
abhimaan9948 <abhimaan8499@gmail.com>
2025-08-26 00:16:11 +0530

    second basic programs

---
+++ basic 2.py
@@ -0,0 +1,38 @@
+# write a table of 30 in a format 30 x 1 = 30
+
+
+
+
+
+
+
+
+
+
+
+
+
+# write a table from 1 to 7 in a format 1 x 1 = 1 and so on make sure after printing 1 table you have to prinnt another table in seperate column
+
+
+
+
+
+
+
+
+#print an array by taking input itself
+
+
+
+
+
+
+
+#after printing an array check and return count of element which is divisble by 2
+
+
+
+
+
+
