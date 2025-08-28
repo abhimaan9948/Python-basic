524dc43f9faf1ce8d634e81231f59e170159192c
abhimaan9948 <abhimaan8499@gmail.com>
2025-08-29 00:12:12 +0530

    sort fn

---
+++ basic 3.py
@@ -0,0 +1,23 @@
+# write an array an sort it via fun
+
+a =[2456789,9087654,34567,98765]
+
+print(a)
+
+
+# print both array before sorting and after sorting
+
+
+a.sort()
+print (a)
+
+
+
+# take an   array sort it and print element line by line in a order like first line
+# contain 0th and 1st index
+# 2nd contain 1st and 2nd index ele
+# 3rd line contain 2nd and 3rd index ele and so on...
+
+print (a[0])
+print (a[1])
+print (a[0],a[1],a[2])
